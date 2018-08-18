41440ee4a78a94b5fad5867f4b39e448226d6171
Dalton Martins <daltonvlm@gmail.com>
2018-08-17 22:47:54 -0300

    Aplica formatacao

--- 05_repeticoes/exercicio_02.c
+++ 05_repeticoes/exercicio_02.c
@@ -10,53 +10,57 @@
 #include <stdio.h>
 #include <math.h>
 
-static int primo(int n){
-    if(2==n){
-        return 1;
-    }
-    if(n<2||!(n%2)){
-        return 0;
-    }
-    int r=sqrt(n);
-    for(int i=3;i<=r;i+=2){
-        if(!(n%i)){
-            return 0;
-        }
-    }
-    return 1;
+static int primo(int n)
+{
+	if (2 == n) {
+		return 1;
+	}
+	if (n < 2 || !(n % 2)) {
+		return 0;
+	}
+	int r = sqrt(n);
+	for (int i = 3; i <= r; i += 2) {
+		if (!(n % i)) {
+			return 0;
+		}
+	}
+	return 1;
 }
 
-void a(void){
-    while(1){
-        int x;
-        printf("x: ");
-        scanf("%d",&x);
-        for(int i=2;i<x;i++){
-            if(primo(i)){
-                printf("%d ",i);
-            }
-        }
-        puts("");
-    }
+void a(void)
+{
+	while (1) {
+		int x;
+		printf("x: ");
+		scanf("%d", &x);
+		for (int i = 2; i < x; i++) {
+			if (primo(i)) {
+				printf("%d ", i);
+			}
+		}
+		puts("");
+	}
 }
 
-void b(void){
-    while(1){
-        int n;
-        printf("n: ");
-        scanf("%d",&n);
-        for(int i=2;n;i++){
-            if(primo(i)){
-                printf("%d ",i);
-                n--;
-            }
-        }
-        puts("");
-    }
+void b(void)
+{
+	while (1) {
+		int n;
+		printf("n: ");
+		scanf("%d", &n);
+		for (int i = 2; n; i++) {
+			if (primo(i)) {
+				printf("%d ", i);
+				n--;
+			}
+		}
+		puts("");
+	}
 }
 
-int main(void){
-    //a();
-    b();
-    return 0;
+int main(void)
+{
+	//a();
+	b();
+	return 0;
 }
