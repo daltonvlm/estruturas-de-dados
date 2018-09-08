81c408fe758aa9cf39a5998b88603acfe78e949b
Dalton Martins <daltonvlm@gmail.com>
2018-09-08 16:58:08 -0300

    Refatoracao

--- 12_tipo_abstrato_de_dados/exercicio_02.c
+++ 12_tipo_abstrato_de_dados/exercicio_02.c
@@ -3,11 +3,6 @@
  * que, dada uma matriz, cria dinamicamente a matriz transposta correspondente.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "matriz.h"
-
 static Matriz *mat_transp(Matriz * mat)
 {
 	int lin = mat_colunas(mat);
@@ -20,46 +15,4 @@ static Matriz *mat_transp(Matriz * mat)
 		}
 	}
 	return tp;
-}
-
-static void mat_imprime(Matriz * mat)
-{
-	int lin = mat_linhas(mat);
-	int col = mat_colunas(mat);
-	for (int i = 0; i < lin; i++) {
-		for (int j = 0; j < col; j++) {
-			printf("%g ", mat_acessa(mat, i, j));
-		}
-		puts("");
-	}
-}
-
-static void mat_popula(Matriz * mat, int max)
-{
-	int lin = mat_linhas(mat);
-	int col = mat_colunas(mat);
-	for (int i = 0; i < lin; i++) {
-		mat_atribui(mat, i, i, rand() % max);
-		for (int j = 0; j < i; j++) {
-			mat_atribui(mat, i, j, rand() % max);
-			mat_atribui(mat, j, i, rand() % max);
-		}
-	}
-}
-
-int main(void)
-{
-	Matriz *mat, *tp;
-	mat = mat_cria(2, 3);
-
-	srand(time(NULL));
-	mat_popula(mat, 10);
-	puts("Matriz:");
-	mat_imprime(mat);
-	tp = mat_transp(mat);
-	puts("\nTransposta:");
-	mat_imprime(tp);
-	mat_libera(mat);
-	mat_libera(tp);
-	return 0;
 }
